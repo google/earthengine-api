4a46c01402d2ee80071b209f38f55d339942fe56
Nate Schmitz <nschmitz@google.com>
2024-05-30 06:45:59 -0700

    Internal changes.
    PiperOrigin-RevId: 638635001

--- javascript/src/apiclient.js
+++ javascript/src/apiclient.js
@@ -24,7 +24,7 @@ const {trustedResourceUrl} = goog.require('safevalues');
 /** @namespace */
 const apiclient = {};
 
-const API_CLIENT_VERSION = '0.1.404';
+const API_CLIENT_VERSION = '0.1.405';
 
 exports.VERSION = apiVersion.VERSION;
 exports.API_CLIENT_VERSION = API_CLIENT_VERSION;
