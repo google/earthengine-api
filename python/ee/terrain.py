54930b6f2e2fd41e98c0c1f151acf1bdf8792fab
Kurt Schwehr <schwehr@google.com>
2024-06-20 07:04:28 -0700

    terrain.py: Add static methods.
    PiperOrigin-RevId: 645017579

--- python/ee/terrain.py
+++ python/ee/terrain.py
@@ -1,6 +1,17 @@
 """A namespace for Terrain."""
+from __future__ import annotations
+
+from typing import Any, Optional, Union
 
 from ee import apifunction
+from ee import computedobject
+from ee import ee_number
+from ee import image as ee_image
+
+_EeBoolType = Union[Any, computedobject.ComputedObject]
+_ImageType = Union[Any, computedobject.ComputedObject]
+_IntegerType = Union[int, ee_number.Number, computedobject.ComputedObject]
+_NumberType = Union[float, ee_number.Number, computedobject.ComputedObject]
 
 
 class Terrain:
@@ -32,3 +43,120 @@ class Terrain:
   @staticmethod
   def name() -> str:
     return 'Terrain'
+
+  @staticmethod
+  def aspect(input: _ImageType) -> ee_image.Image:
+    """Returns an ee.Image with aspect in degrees from a terrain DEM.
+
+    The local gradient is computed using the 4-connected neighbors of each
+    pixel, so missing values will occur around the edges of an image.
+
+    Args:
+      input: An elevation image, in meters.
+    """
+
+    return apifunction.ApiFunction.call_('Terrain.aspect', input)
+
+  @staticmethod
+  def fillMinima(
+      image: _ImageType,
+      borderValue: Optional[_IntegerType] = None,
+      neighborhood: Optional[_IntegerType] = None,
+  ) -> ee_image.Image:
+    """Returns an ee.Image with local minima filled.
+
+    Only works on INT types.
+
+    Args:
+      image: The image to fill.
+      borderValue: The border value.
+      neighborhood: The size of the neighborhood to compute over.
+    """
+
+    return apifunction.ApiFunction.call_(
+        'Terrain.fillMinima', image, borderValue, neighborhood
+    )
+
+  @staticmethod
+  def hillShadow(
+      image: _ImageType,
+      azimuth: _NumberType,
+      zenith: _NumberType,
+      neighborhoodSize: Optional[_IntegerType] = None,
+      hysteresis: Optional[_EeBoolType] = None,
+  ) -> ee_image.Image:
+    """Returns an ee.Image with the hill shadow.
+
+    Creates a shadow band, with output 1 where pixels are illumunated and 0
+    where they are shadowed. Takes as input an elevation band, azimuth and
+    zenith of the light source in degrees, a neighborhood size, and whether or
+    not to apply hysteresis when a shadow appears. Currently, this algorithm
+    only works for Mercator projections, in which light rays are parallel.
+
+    Args:
+      image: The image to which to apply the shadow algorithm, in which each
+        pixel should represent an elevation in meters.
+      azimuth: Azimuth in degrees.
+      zenith: Zenith in degrees.
+      neighborhoodSize: Neighborhood size.
+      hysteresis: Use hysteresis. Less physically accurate, but may generate
+        better images.
+    """
+
+    return apifunction.ApiFunction.call_(
+        'Terrain.hillShadow',
+        image,
+        azimuth,
+        zenith,
+        neighborhoodSize,
+        hysteresis,
+    )
+
+  @staticmethod
+  def hillshade(
+      input: _ImageType,
+      azimuth: Optional[_NumberType] = None,
+      elevation: Optional[_NumberType] = None,
+  ) -> ee_image.Image:
+    """Returns an ee.Image with a simple hillshade from a DEM.
+
+    Args:
+      input: An elevation image, in meters.
+      azimuth: The illumination azimuth in degrees from north.
+      elevation: The illumination elevation in degrees.
+    """
+
+    return apifunction.ApiFunction.call_(
+        'Terrain.hillshade', input, azimuth, elevation
+    )
+
+  @staticmethod
+  def products(input: _ImageType) -> ee_image.Image:
+    """Returns slope, aspect, and a simple hillshade from a terrain DEM.
+
+    Expects an image containing either a single band of elevation, measured in
+    meters, or if there's more than one band, one named 'elevation'. Adds output
+    bands named 'slope' and 'aspect' measured in degrees plus an unsigned byte
+    output band named 'hillshade' for visualization. All other bands and
+    metadata are copied from the input image. The local gradient is computed
+    using the 4-connected neighbors of each pixel, so missing values will occur
+    around the edges of an image.
+
+    Args:
+      input: An elevation image, in meters.
+    """
+
+    return apifunction.ApiFunction.call_('Terrain.products', input)
+
+  @staticmethod
+  def slope(input: _ImageType) -> ee_image.Image:
+    """Returns slope in degrees from a terrain DEM.
+
+    The local gradient is computed using the 4-connected neighbors of each
+    pixel, so missing values will occur around the edges of an image.
+
+    Args:
+      input: An elevation image, in meters.
+    """
+
+    return apifunction.ApiFunction.call_('Terrain.slope', input)
---
+++ python/ee/tests/terrain_test.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+"""Tests for the ee.Terrain module."""
+
+import json
+from typing import Any, Dict
+
+import unittest
+import ee
+from ee import apitestcase
+
+# For an ee.Image(1).
+IMAGE_1 = 1
+IMAGE_EXPRESSION_1 = {
+    'functionInvocationValue': {
+        'functionName': 'Image.constant',
+        'arguments': {'value': {'constantValue': IMAGE_1}},
+    }
+}
+
+
+def make_expression_graph(
+    function_invocation_value: Dict[str, Any],
+) -> Dict[str, Any]:
+  return {
+      'result': '0',
+      'values': {'0': {'functionInvocationValue': function_invocation_value}},
+  }
+
+
+class TerrainTest(apitestcase.ApiTestCase):
+
+  def test_aspect(self):
+    expect = make_expression_graph({
+        'functionName': 'Terrain.aspect',
+        'arguments': {'input': IMAGE_EXPRESSION_1},
+    })
+    expression = ee.Terrain.aspect(IMAGE_1)
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+    expression = ee.Terrain.aspect(input=IMAGE_1)
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+  def test_fill_minima(self):
+    border_value = 2
+    neighborhood = 3
+    expect = make_expression_graph({
+        'functionName': 'Terrain.fillMinima',
+        'arguments': {
+            'image': IMAGE_EXPRESSION_1,
+            'borderValue': {'constantValue': border_value},
+            'neighborhood': {'constantValue': neighborhood},
+        },
+    })
+    expression = ee.Terrain.fillMinima(IMAGE_1, border_value, neighborhood)
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+    expression = ee.Terrain.fillMinima(
+        image=IMAGE_1, borderValue=border_value, neighborhood=neighborhood
+    )
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+  def test_hill_shadow(self):
+    azimuth = 2
+    zenith = 3
+    neighborhood_size = 4
+    hysteresis = False
+    expect = make_expression_graph({
+        'functionName': 'Terrain.hillShadow',
+        'arguments': {
+            'image': IMAGE_EXPRESSION_1,
+            'azimuth': {'constantValue': azimuth},
+            'zenith': {'constantValue': zenith},
+            'neighborhoodSize': {'constantValue': neighborhood_size},
+            'hysteresis': {'constantValue': hysteresis},
+        },
+    })
+    expression = ee.Terrain.hillShadow(
+        IMAGE_1, azimuth, zenith, neighborhood_size, hysteresis
+    )
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+    expression = ee.Terrain.hillShadow(
+        image=IMAGE_1,
+        azimuth=azimuth,
+        zenith=zenith,
+        neighborhoodSize=neighborhood_size,
+        hysteresis=hysteresis,
+    )
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+  def test_hillshade(self):
+    azimuth = 2
+    elevation = 3
+    expect = make_expression_graph({
+        'functionName': 'Terrain.hillshade',
+        'arguments': {
+            'input': IMAGE_EXPRESSION_1,
+            'azimuth': {'constantValue': azimuth},
+            'elevation': {'constantValue': elevation},
+        },
+    })
+    expression = ee.Terrain.hillshade(IMAGE_1, azimuth, elevation)
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+    expression = ee.Terrain.hillshade(
+        input=IMAGE_1, azimuth=azimuth, elevation=elevation
+    )
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+  def test_products(self):
+    expect = make_expression_graph({
+        'functionName': 'Terrain.products',
+        'arguments': {'input': IMAGE_EXPRESSION_1},
+    })
+    expression = ee.Terrain.products(IMAGE_1)
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+    expression = ee.Terrain.products(input=IMAGE_1)
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+  def test_slope(self):
+    expect = make_expression_graph({
+        'functionName': 'Terrain.slope',
+        'arguments': {'input': IMAGE_EXPRESSION_1},
+    })
+    expression = ee.Terrain.slope(IMAGE_1)
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+    expression = ee.Terrain.slope(input=IMAGE_1)
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+
+if __name__ == '__main__':
+  unittest.main()
