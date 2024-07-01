9dc41f1ce18f18081f9f2fecc174debe2bf49b2d
Kurt Schwehr <schwehr@google.com>
2024-07-01 01:38:33 -0700

    imagecollection.py: Add static methods
    PiperOrigin-RevId: 648284082

--- python/ee/imagecollection.py
+++ python/ee/imagecollection.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Any, Callable, Dict, Optional, Sequence, Tuple
+from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
 
 from ee import _utils
 from ee import apifunction
@@ -10,10 +10,39 @@ from ee import collection
 from ee import computedobject
 from ee import data
 from ee import deprecation
+from ee import dictionary
 from ee import ee_exception
 from ee import ee_list
+from ee import ee_number
+from ee import ee_string
 from ee import ee_types
 from ee import image
+from ee import projection
+from ee import reducer
+
+_EeBoolType = Union[Any, computedobject.ComputedObject]
+_DictionaryType = Union[
+    Dict[Any, Any],
+    Sequence[Any],
+    'dictionary.Dictionary',
+    computedobject.ComputedObject,
+]
+_GeometryType = Union[Any, computedobject.ComputedObject]
+# The full range in inputs for an ImageCollection argument is large.
+_ImageCollectionType = Union[
+    Any, 'ImageCollection', computedobject.ComputedObject
+]
+_IntegerType = Union[int, ee_number.Number, computedobject.ComputedObject]
+_ListType = Union[List[Any], Tuple[Any, Any], computedobject.ComputedObject]
+_NumberType = Union[float, ee_number.Number, computedobject.ComputedObject]
+_ProjectionType = Union[
+    str,
+    'ee_string.String',
+    projection.Projection,
+    computedobject.ComputedObject,
+]
+_ReducerType = Union[reducer.Reducer, computedobject.ComputedObject]
+_StringType = Union[str, ee_string.String, computedobject.ComputedObject]
 
 
 class ImageCollection(collection.Collection):
@@ -365,3 +394,232 @@ class ImageCollection(collection.Collection):
     # behaviour, so we do nothing.
     return self._apply_preparation_function(image.Image.prepare_for_export,
                                             params)
+
+  def cast(
+      self,
+      bandTypes: _DictionaryType,  # pylint: disable=invalid-name
+      bandOrder: _ListType,  # pylint: disable=invalid-name
+  ) -> ImageCollection:
+    """Casts some or all bands of each image to the specified types.
+
+    Args:
+      bandTypes: A dictionary from band name to band types. Types can be
+        PixelTypes or strings. The valid strings are: 'int8', 'int16', 'int32',
+        'int64', 'uint8', 'uint16', 'uint32', 'byte', 'short', 'int', 'long',
+        'float', and 'double'. Must include all bands already in any image in
+        the collection. If this includes bands that are not already in an input
+        image, they will be added to the image as transparent bands.
+      bandOrder: A list specifying the order of the bands in the result. Must
+        match the keys of bandTypes.
+
+    Returns:
+      An ee.ImageCollection.
+    """
+
+    return apifunction.ApiFunction.call_(
+        self.name() + '.cast', self, bandTypes, bandOrder
+    )
+
+  def combine(
+      self,
+      secondary: _ImageCollectionType,
+      overwrite: Optional[_EeBoolType] = None,
+  ) -> ImageCollection:
+    """Returns a collection adding all the bands from the image in secondary.
+
+    Makes a new collection that is a copy of the images in primary, adding all
+    the bands from the image in secondary with a matching ID. If there are no
+    matching IDs, the resulting collection will be empty. This is equivalent to
+    an inner join on ID with merging of the bands of the result.
+
+    Note that this algorithm assumes that for a matching pair of inputs, both
+    have the same footprint and metadata.
+
+    Args:
+      secondary: The secondary collection to join.
+      overwrite: If true, bands with the same name will get overwritten. If
+        false, bands with the same name will be renamed.
+    """
+
+    return apifunction.ApiFunction.call_(
+        self.name() + '.combine', self, secondary, overwrite
+    )
+
+  def formaTrend(
+      self,
+      covariates: Optional[_ImageCollectionType] = None,
+      windowSize: Optional[_IntegerType] = None,  # pylint: disable=invalid-name
+  ) -> image.Image:
+    """Returns an image with the forma trend of the collection.
+
+    Computes the long and short term trends of a time series or optionally, the
+    trends of the ratio of the time series and a covariate. The long term trend
+    is estimated from the linear term of a regression on the full time series.
+    The short term trend is computed as the windowed minimum over the time
+    series.
+
+    The time series and covariate series are expected to contain a single band
+    each, and the time series is expected to be evenly spaced in time. The
+    output is 4 float bands: the long and short term trends, the t-test of the
+    long term trend against the time series, and the Bruce Hansen test of
+    parameter stability.
+
+    Args:
+      covariates: Cofactors to use in the trend analysis.
+      windowSize: Short term trend analysis window size, in images.
+    """
+
+    return apifunction.ApiFunction.call_(
+        self.name() + '.formaTrend', self, covariates, windowSize
+    )
+
+  @staticmethod
+  def fromImages(images: _ListType) -> ImageCollection:
+    """Returns the image collection containing the given images."""
+
+    return apifunction.ApiFunction.call_('ImageCollection.fromImages', images)
+
+  def getRegion(
+      self,
+      geometry: _GeometryType,
+      scale: Optional[_NumberType] = None,
+      crs: Optional[_ProjectionType] = None,
+      crsTransform: Optional[_ListType] = None,  # pylint: disable=invalid-name
+  ) -> ee_list.List:
+    """Returns a list of values for each [pixel, band, image] tuple.
+
+    Output an array of values for each [pixel, band, image] tuple in an
+    ImageCollection. The output contains rows of id, lon, lat, time, and all
+    bands for each image that intersects each pixel in the given region.
+    Attempting to extract more than 1048576 values will result in an error.
+
+    Args:
+      geometry: The region over which to extract data.
+      scale: A nominal scale in meters of the projection to work in.
+      crs: The projection to work in. If unspecified, defaults to EPSG:4326. If
+        specified in addition to scale, the projection is rescaled to the
+        specified scale.
+      crsTransform: The array of CRS transform values. This is a row-major
+        ordering of a 3x2 affine transform. This option is mutually exclusive
+        with the scale option, and will replace any transform already set on the
+        given projection.
+    """
+
+    return apifunction.ApiFunction.call_(
+        self.name() + '.getRegion', self, geometry, scale, crs, crsTransform
+    )
+
+  @staticmethod
+  def load(
+      id: _StringType, version: Optional[_IntegerType] = None
+  ) -> ImageCollection:
+    """Returns the image collection given its ID.
+
+    Args:
+      id: The asset ID of the image collection.
+      version: The version of the asset. -1 signifies the latest version.
+    """
+
+    return apifunction.ApiFunction.call_('ImageCollection.load', id, version)
+
+  def merge(self, collection2: _ImageCollectionType) -> ImageCollection:
+    """Returns a collection of two image collections merged into one.
+
+    The result has all the images that were in either collection.
+
+    Args:
+      collection2: The second collection to merge.
+    """
+
+    return apifunction.ApiFunction.call_(
+        self.name() + '.merge', self, collection2
+    )
+
+  def mosaic(self) -> image.Image:
+    """Composites all the images in a collection, using the mask."""
+
+    return apifunction.ApiFunction.call_(self.name() + '.mosaic', self)
+
+  def qualityMosaic(
+      self, qualityBand: _StringType  # pylint: disable=invalid-name
+  ) -> image.Image:
+    """Returns an image with the quality mosaic of the collection.
+
+    Composites all the images in a collection, using a quality band as a
+    per-pixel ordering function.
+
+    Args:
+      qualityBand: The name of the quality band in the collection.
+    """
+
+    return apifunction.ApiFunction.call_(
+        self.name() + '.qualityMosaic', self, qualityBand
+    )
+
+  def reduce(
+      self,
+      reducer: _ReducerType,
+      # pylint: disable-next=invalid-name
+      parallelScale: Optional[_NumberType] = None,
+  ) -> image.Image:
+    """Returns a reduced image from the collection.
+
+    Applies a reducer across all of the images in a collection.
+
+    If the reducer has a single input, it will be applied separately to each
+    band of the collection; otherwise it must have the same number of inputs as
+    the collection has bands.
+
+    The reducer output names determine the names of the output bands: reducers
+    with multiple inputs will use the output names directly, while reducers with
+    a single input will prefix the output name with the input band name (e.g.,
+    '10_mean', '20_mean').
+
+    Args:
+      reducer: The reducer to apply to the given collection.
+      parallelScale: A scaling factor used to limit memory use; using a larger
+        parallelScale (e.g., 2 or 4) may enable computations that run out of
+        memory with the default.
+    """
+
+    return apifunction.ApiFunction.call_(
+        self.name() + '.reduce', self, reducer, parallelScale
+    )
+
+  def toArray(self) -> image.Image:
+    """Returns an image of an image collection converted into 2D arrays.
+
+    At each pixel, the images that have valid (unmasked) values in all bands are
+    laid out along the first axis of the array in the order they appear in the
+    image collection. The bands of each image are laid out along the second axis
+    of the array, in the order the bands appear in that image. The array element
+    type will be the union of the types of each band.
+    """
+
+    return apifunction.ApiFunction.call_(self.name() + '.toArray', self)
+
+  def toArrayPerBand(self, axis: Optional[_IntegerType] = None) -> image.Image:
+    """Returns an image of an image collection converted into 2D arrays.
+
+    Concatenates multiple images into a single array image. The result will be
+    masked if any input is masked.
+
+    Args:
+      axis: Axis to concatenate along; must be at least 0 and at most the
+        minimum dimension of any band in the collection.
+    """
+
+    return apifunction.ApiFunction.call_(
+        self.name() + '.toArrayPerBand', self, axis
+    )
+
+  def toBands(self) -> image.Image:
+    """Returns an image containing all bands of every image in the collection.
+
+    Converts a collection to a single multi-band image containing all of the
+    bands of every image in the collection. Output bands are named by prefixing
+    the existing band names with the image id from which it came (e.g.,
+    'image1_band1'). Note: The maximum number of bands is 5000.
+    """
+
+    return apifunction.ApiFunction.call_(self.name() + '.toBands', self)
