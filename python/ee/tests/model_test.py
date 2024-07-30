864d62166bc6e466ef01786be3fe2962f473886e
Kurt Schwehr <schwehr@google.com>
2024-07-30 16:26:36 -0700

    model_test.py: Add serialization test for `fromAiPlatformPredictor`.
    PiperOrigin-RevId: 657769213

--- python/ee/tests/model_test.py
+++ python/ee/tests/model_test.py
@@ -62,7 +62,164 @@ class ModelTest(apitestcase.ApiTestCase):
     }
     self.assertEqual(expect, result)
 
-  # TODO: test_from_ai_platform_predictor
+  def test_from_ai_platform_predictor(self):
+    project_name = 'some project'
+    project_id = 'a project id'
+    model_name = 'model name'
+    version = 'a version'
+    region = 'us-central1'
+    input_properties = ['a', 'b']
+    input_type_override = {'c': ee.PixelType.int8()}
+    input_shapes = {'d': [1, 2]}
+    proj = 'epsg:4326'
+    fix_input_proj = True
+    input_tile_size = [3, 4]
+    input_overlap_size = [5, 6]
+    output_tile_size = [7, 8]
+    output_bands = {'e': {'type': ee.PixelType.int16(), 'dimensions': 10}}
+    output_properties = {'f': {'type': ee.PixelType.int32(), 'dimensions': 11}}
+    output_multiplier = 9.1
+
+    expect = make_expression_graph({
+        'functionName': 'Model.fromAiPlatformPredictor',
+        'arguments': {
+            'fixInputProj': {'constantValue': fix_input_proj},
+            'inputOverlapSize': {'constantValue': input_overlap_size},
+            'inputProperties': {'constantValue': input_properties},
+            'inputShapes': {'constantValue': input_shapes},
+            'inputTileSize': {'constantValue': input_tile_size},
+            'inputTypeOverride': {
+                'dictionaryValue': {
+                    'values': {
+                        'c': {
+                            'functionInvocationValue': {
+                                'functionName': 'PixelType',
+                                'arguments': {
+                                    'precision': {
+                                        'functionInvocationValue': {
+                                            'functionName': 'PixelType.int8',
+                                            'arguments': {},
+                                        }
+                                    }
+                                },
+                            }
+                        }
+                    }
+                }
+            },
+            'modelName': {'constantValue': model_name},
+            'outputBands': {
+                'dictionaryValue': {
+                    'values': {
+                        'e': {
+                            'dictionaryValue': {
+                                'values': {
+                                    'dimensions': {'constantValue': 10},
+                                    'type': {
+                                        'functionInvocationValue': {
+                                            'functionName': 'PixelType',
+                                            'arguments': {
+                                                'precision': {
+                                                    'functionInvocationValue': {
+                                                        'functionName': (
+                                                            'PixelType.int16'
+                                                        ),
+                                                        'arguments': {},
+                                                    }
+                                                }
+                                            },
+                                        }
+                                    },
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            'outputMultiplier': {'constantValue': output_multiplier},
+            'outputProperties': {
+                'dictionaryValue': {
+                    'values': {
+                        'f': {
+                            'dictionaryValue': {
+                                'values': {
+                                    'dimensions': {'constantValue': 11},
+                                    'type': {
+                                        'functionInvocationValue': {
+                                            'functionName': 'PixelType',
+                                            'arguments': {
+                                                'precision': {
+                                                    'functionInvocationValue': {
+                                                        'functionName': (
+                                                            'PixelType.int32'
+                                                        ),
+                                                        'arguments': {},
+                                                    }
+                                                }
+                                            },
+                                        }
+                                    },
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            'outputTileSize': {'constantValue': output_tile_size},
+            'proj': {
+                'functionInvocationValue': {
+                    'functionName': 'Projection',
+                    'arguments': {'crs': {'constantValue': 'epsg:4326'}},
+                }
+            },
+            'projectId': {'constantValue': project_id},
+            'projectName': {'constantValue': project_name},
+            'region': {'constantValue': region},
+            'version': {'constantValue': version},
+        },
+    })
+    expression = ee.Model.fromAiPlatformPredictor(
+        project_name,
+        project_id,
+        model_name,
+        version,
+        region,
+        input_properties,
+        input_type_override,
+        input_shapes,
+        proj,
+        fix_input_proj,
+        input_tile_size,
+        input_overlap_size,
+        output_tile_size,
+        output_bands,
+        output_properties,
+        output_multiplier,
+    )
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
+    expression = ee.Model.fromAiPlatformPredictor(
+        projectName=project_name,
+        projectId=project_id,
+        modelName=model_name,
+        version=version,
+        region=region,
+        inputProperties=input_properties,
+        inputTypeOverride=input_type_override,
+        inputShapes=input_shapes,
+        proj=proj,
+        fixInputProj=fix_input_proj,
+        inputTileSize=input_tile_size,
+        inputOverlapSize=input_overlap_size,
+        outputTileSize=output_tile_size,
+        outputBands=output_bands,
+        outputProperties=output_properties,
+        outputMultiplier=output_multiplier,
+    )
+    result = json.loads(expression.serialize())
+    self.assertEqual(expect, result)
+
   # TODO: test_from_vertex_ai
 
   def test_predict_image(self):
