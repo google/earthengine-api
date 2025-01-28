b4321b91f7bdad099c237f14a613295e4377b9a2
jgarcia525 <jorgeegarcia@google.com>
2025-01-27 18:47:18 -0600

    Update version numbers for v1.5.1rc0

--- javascript/src/apiclient.js
+++ javascript/src/apiclient.js
@@ -24,7 +24,7 @@ const {trustedResourceUrl} = goog.require('safevalues');
 /** @namespace */
 const apiclient = {};
 
-const API_CLIENT_VERSION = '0.0.0';
+const API_CLIENT_VERSION = '1.5.1';
 
 exports.VERSION = apiVersion.VERSION;
 exports.API_CLIENT_VERSION = API_CLIENT_VERSION;
