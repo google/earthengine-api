a13c88ef1477d18d0fcbdcaa19d074dfc51c13f4
Kurt Schwehr <schwehr@google.com>
2025-12-20 05:00:00 -0800

    Add test coverage for ee_types.py and make `Object` a string
    PiperOrigin-RevId: 847086610

--- python/ee/tests/ee_types_test.py
+++ python/ee/tests/ee_types_test.py
@@ -4,12 +4,13 @@
 import datetime
 
 import unittest
+from ee import apitestcase
 from ee import ee_list
 from ee import ee_number
 from ee import ee_types
 
 
-class EeTypesTest(unittest.TestCase):
+class EeTypesTest(apitestcase.ApiTestCase):
 
   def test_class_to_name(self):
     self.assertEqual('List', ee_types.classToName(ee_list.List))
@@ -26,6 +27,22 @@ class EeTypesTest(unittest.TestCase):
       pass
 
     self.assertEqual('Object', ee_types.classToName(Foo))
+
+  def test_is_subtype(self):
+    self.assertTrue(ee_types.isSubtype('Image', 'Image'))
+    self.assertTrue(ee_types.isSubtype('Element', 'Image'))
+    self.assertTrue(ee_types.isSubtype('Collection', 'ImageCollection'))
+    self.assertTrue(ee_types.isSubtype('FeatureCollection', 'ImageCollection'))
+    self.assertTrue(ee_types.isSubtype('Object', 'Image'))
+    self.assertFalse(ee_types.isSubtype('Image', 'Element'))
+
+  def test_is_array(self):
+    self.assertTrue(ee_types.isArray([]))
+    self.assertTrue(ee_types.isArray(()))
+    self.assertTrue(ee_types.isArray(ee_list.List([1, 2])))
+    self.assertFalse(ee_types.isArray(1))
+    self.assertFalse(ee_types.isArray('string'))
+    self.assertFalse(ee_types.isArray(ee_number.Number(1)))
 
 
 if __name__ == '__main__':
