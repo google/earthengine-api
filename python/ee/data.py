7f00e12f1b76ce8ced9e1e4e77d89d14fea4e0d8
Kurt Schwehr <schwehr@google.com>
2026-02-23 11:19:02 -0800

    data.py: Debug issue in  get_persistent_credentials
    PiperOrigin-RevId: 874167872

--- python/ee/data.py
+++ python/ee/data.py
@@ -231,7 +231,7 @@ def get_persistent_credentials() -> credentials_lib.Credentials:
     # earthengine set_project always overrides gcloud set-quota-project
     project = args.get('quota_project_id') or oauth.get_appdefault_project()
     if project and project != credentials.quota_project_id:
-      credentials = credentials.with_quota_project(project)
+      credentials = credentials.with_quota_project(project)  # What is up here?
     if oauth.is_valid_credentials(credentials):
       return credentials
   raise ee_exception.EEException(  # pylint: disable=raise-missing-from
